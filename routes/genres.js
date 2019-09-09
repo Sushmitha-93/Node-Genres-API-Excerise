2a7ddb1e3df4cfd7f5fc7e55e96cb06199ecc84a
Sushmitha DT <dtsushmitha@gmail.com>
2019-09-09 20:23:25 +0530

    Handled typeError when invalid mongoDB id was given to API

--- routes/genres.js
+++ routes/genres.js
@@ -28,7 +28,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   //Check if id exists
   const genre = await Genres.findById(req.params.id).catch(err => {
-    return res.status(404).send("Genre ID does not exist - Invlid id");
+    //  catch Invlid id format error. Because it thorows Type cast error if ID is not of MongoDB format.
   });
   if (!genre) return res.status(404).send("Genre ID does not exist");
 
@@ -74,13 +74,13 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   //Check if id exists
-  let genre = await Genres.findByIdAndDelete(req.params.id).catch(err =>
-    res.status(404).send("Genre ID does not exist - Invlid id")
-  );
+  let genre = await Genres.findByIdAndDelete(req.params.id).catch(err => {
+    //  catch Invlid id format error. Because it thorows Type cast error if ID is not of MongoDB format
+  });
   if (!genre) return res.status(404).send("Genre ID does not exist");
 
   //Delete and send response
-  // genre = await Genres.findByIdAndDelete(req.params.id);
+  genre = await Genres.findByIdAndDelete(req.params.id);
 
   res.send(genre);
 });
