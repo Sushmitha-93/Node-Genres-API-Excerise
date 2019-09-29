1671bd9f2127e7032c34cd6ce4f8d0ea5cbdeada
Sushmitha DT <dtsushmitha@gmail.com>
2019-09-29 18:44:00 +0530

    Logging into MongoDB

--- logger.js
+++ logger.js
@@ -1,9 +1,11 @@
 const winston = require("winston");
+require("winston-mongodb");
 
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs.log" })
+    new winston.transports.File({ filename: "logs.log" }),
+    new winston.transports.MongoDB({ db: "mongodb://localhost/vidlydb" })
   ]
 });
 
